9f4b442ec1684efca34a7006cb0148ed6b9b471c
Sorokina-Alina <alina9551@icloud.com>
2024-02-28 19:13:00 +0300

    DB done

---
+++ db/migrations/20240228122030-create-product.js
@@ -0,0 +1,44 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      img: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      count: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  },
+};
---
+++ db/seeders/20240228155333-Product.js
@@ -0,0 +1,47 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Products', [
+      {
+        title: 'Никоретте таблетки для рассасывания мятные 2мг, №20',
+        img: 'https://static.vitaexpress.ru/public/images/webp_large_w/181361.webp',
+        description: 'Лечение табачной зависимости путем снижения потребности в никотине и облегчения симптомов «отмены», возникающих при отказе от курения.',
+        price: 799,
+        count: 15,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'ВитаМишки Immuno+ пастилки жевательные, №30',
+        img: 'https://static.vitaexpress.ru/public/images/webp_large_w/153463.webp',
+        description: 'Рекомендуется в качестве биологически активной добавки к пище - дополнительного источника витаминов С, Е, Д, цинка, селена для детей.',
+        price: 807,
+        count: 27,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Vitascience Фитопассит капсулы, №30',
+        img: 'https://static.vitaexpress.ru/public/images/webp_large_w/175507.webp',
+        description: 'Способствует защите организма от стресса. ',
+        price: 379,
+        count: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Парадиз Ультра лосьон педикулицидное средство, 100мл',
+        img: 'https://static.vitaexpress.ru/public/images/webp_large_w/192815.webp',
+        description: 'Действующие вещества — диметикон-350 и изопарафины. Диметикон равномерно распределяется по волосистой части головы, обволакивает насекомых, закупоривает их дыхательные отверстия, перекрывая доступ кислорода. В результате вши начинают погибать уже на первой минуте. Средство действует на все формы — зрелые особи, личинки и гниды. Поэтому достаточно одного применения для уверенной победы над вшами и избавления от зуда. Не раздражает кожу и не всасывается, не имеет запаха, относится к малоопасным соединениям.',
+        price: 279,
+        count: 60,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Products', null, {});
+  },
+};
