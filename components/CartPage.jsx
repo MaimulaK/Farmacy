d30966adbe78a9f822ca28ba89b61d27be68e973
Sorokina-Alina <alina9551@icloud.com>
2024-03-01 11:04:52 +0300

    gg

--- components/CartPage.jsx
+++ components/CartPage.jsx
@@ -1,15 +1,13 @@
 const React = require('react');
 const Layout = require('./Layout');
-// const CartItem = require('./CartItem');
+const CartItem = require('./CartItem');
 
-function CartPage({ title }) {
+function CartPage({ title, products }) {
   return (
     <Layout title={title}>
       <h2>Корзина</h2>
       <h4>Вы выбрали:</h4>
-      <div className='cart-container'>
-        {/* <CartItem /> */}
-      </div>
+      <div className='cart-container' />
     </Layout>
   );
 }
--- routes/api/cart.api.router.js
+++ routes/api/cart.api.router.js
@@ -1,21 +1,22 @@
 const router = require('express').Router();
-const { Cart } = require('../../db/models');
-const { CartItem } = require('../../db/models');
-const ProductCard = require('../../components/ProductCard');
+const { CartItem, Cart } = require('../../db/models');
+const CartItemJsx = require('../../components/CartItem');
 
 router.post('/', async (req, res) => {
   try {
     const { id } = req.body;
+    console.log("id", id);
     const { user } = res.locals;
-    const cart = await Cart.findOrCreate({ where: user.id });
+    console.log(user);
+    const cart = await Cart.findOrCreate({ where:{ userId: user.id} });
     if (cart) {
-      let cartItem = CartItem.findOne({ where: user.id, productId: id });
+      let cartItem = CartItem.findOne({ where:{userId: user.id, productId: id } });
       if (cartItem) {
         const result = cartItem.update({ count: cartItem + 1 }, { cartItem });
         if (result) {
           cartItem = CartItem.findOne({ where: cartItem.id });
-          const html = res.renderComponent(ProductCard, {}, { doctype: false });
-          res.json();
+          const html = res.renderComponent(CartItemJsx, {}, { doctype: false });
+          res.status(201).json({ message: 'success', html });
         }
       }
     }
